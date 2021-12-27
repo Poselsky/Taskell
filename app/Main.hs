3a1b0cfb3b32d08c3d4d305c7ca369b7e360bbde
Poselsky <oleg.musijenko@seznam.cz>
2021-12-27 20:59:24 +0100

    update repl

--- app/Main.hs
+++ app/Main.hs
@@ -2,25 +2,49 @@ module Main where
 
 import Parsing.Parser
 import Parsing.Syntax
+import LLVMIR.Emit
+import LLVMIR.Codegen
 
 import Control.Monad.Trans
 import System.Console.Haskeline
+import System.Environment
+import System.IO
 import Text.Parsec
 
-process :: String -> IO ()
-process line = do
-  let res = parseToplevel line
-  case res of
-    Left err -> do print err
-    Right ex -> do 
-        print $ length ex
-        mapM_ print ex
+import qualified LLVM.AST as AST
 
-main :: IO ()
-main = runInputT defaultSettings loop
+initModule :: AST.Module
+initModule = emptyModule "my cool jit"
+
+process :: AST.Module -> String -> IO (Maybe AST.Module)
+process modo source = do
+  let res = parseToplevel source
+  case res of
+    Left err -> print err >> return Nothing
+    Right ex -> do
+      print res
+      ast <- codegen modo ex
+      return $ Just ast
+
+processFile :: String -> IO (Maybe AST.Module)
+processFile fname = readFile fname >>= process initModule
+
+repl :: IO ()
+repl = runInputT defaultSettings (loop initModule)
   where
-  loop = do
+  loop mod = do
     minput <- getInputLine "ready> "
     case minput of
       Nothing -> outputStrLn "Goodbye."
-      Just input -> (liftIO $ process input) >> loop
+      Just input -> do
+        modn <- liftIO $ process mod input
+        case modn of
+          Just modn -> loop modn
+          Nothing -> loop mod
+
+main :: IO ()
+main = do
+  args <- getArgs
+  case args of
+    []      -> repl
+    [fname] -> processFile fname >> return ()
